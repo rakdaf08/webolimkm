1e4a141ddb11e018ab45f22ea60c12b874f2c35c
rakadaffa <19623249@std.stei.itb.ac.id>
2024-08-16 15:55:23 +0700

    first commit

---
+++ website-olim-km/Components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="border-2 border-black flexBetween max-container padding-container relative z-30 py-5">
+      Navbar
+    </nav>
+  );
+};
+
+export default Navbar;
