1fd0a5270db695135175415d3dd1e3fc398ce84a
rakadaffa <19623249@std.stei.itb.ac.id>
2024-08-18 09:35:03 +0700

    commit2

--- website-olim-km/Components/Navbar.tsx
+++ website-olim-km/Components/Navbar.tsx
@@ -1,9 +1,34 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { NAV_LINKS } from "@/constants";
 
 const Navbar = () => {
   return (
-    <nav className="border-2 border-black flexBetween max-container padding-container relative z-30 py-5">
-      Navbar
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/logo-olim-km.ico" alt="logo" width={74} height={29} />
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+          >
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={32}
+        height={32}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   );
 };
---
+++ website-olim-km/constants/index.ts
@@ -0,0 +1,7 @@
+//Navigation
+export const NAV_LINKS = [
+  {href: '/', key: 'home', label: 'Home'},
+  {href: '/', key: 'schedule', label: 'Schedule'},
+  {href: '/', key: 'leaderboard', label: 'Leaderboard'},
+  {href: '/', key: 'players', label: 'Players'},
+];
