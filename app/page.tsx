694592b30cd38fb2e0701215752b592beb930222
rakadaffa <19623249@std.stei.itb.ac.id>
2024-08-24 22:27:18 +0700

    file changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Footer from "@/Components/Footer";
 import Navbar from "@/Components/Navbar";
+import MainPage from "./Home/page";
 
 export default function Home() {
-  return <div>MAIN PAGE</div>;
+  return (
+    <div>
+      <MainPage />
+    </div>
+  );
 }
