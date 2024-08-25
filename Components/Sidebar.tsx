7707d51003218a1dd721aa4dc1cadc29ec27fee2
rakadaffa <19623249@std.stei.itb.ac.id>
2024-08-25 12:51:13 +0700

    add:sidebar and icon

--- Components/Sidebar.tsx
+++ Components/Sidebar.tsx
@@ -1,35 +1,58 @@
 "use client";
 
 import React, { useState } from "react";
-import Image from "next/image";
 import { NAV_LINKS } from "@/constants";
 import Link from "next/link";
+import Image from "next/image";
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className={`lg:hidden`}>
-      <Image
-        src="menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer"
-        onClick={() => setIsOpen(!isOpen)}
-      />
-      <div>
-        <ul className="grid">
-          {NAV_LINKS.map((link) => (
-            <Link
-              href={link.href}
-              key={link.key}
-              className="text-base text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold"
-            >
-              {link.label}
-            </Link>
-          ))}
-        </ul>
+    <div className="transition-all xs:flex lg:hidden">
+      <div className="flex justify-center">
+        <div className="">
+          <Image
+            src="/menu.svg"
+            alt="menu"
+            width={32}
+            height={32}
+            className="inline-block cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          />
+        </div>
       </div>
+
+      {isOpen && (
+        <div>
+          <div className="fixed top-0 right-0 z-10 h-screen p-4 overflow-y-auto transition ease-in-out duration-100 bg-white border border-black">
+            <div className="flex justify-between items-center">
+              <span className="text-base font-semibold text-gray-600">
+                MENU
+              </span>
+              <span
+                onClick={() => setIsOpen(!isOpen)}
+                className="cursor-pointer font-bold text-xl"
+              >
+                x
+              </span>
+            </div>
+
+            <div className="py-4">
+              <ul className="grid gap-4">
+                {NAV_LINKS.map((link) => (
+                  <Link
+                    href={link.href}
+                    key={link.key}
+                    className="text-base text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold"
+                  >
+                    {link.label}
+                  </Link>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/Components/Footer";
 import Navbar from "@/Components/Navbar";
 import MainPage from "./Home/page";
+import Sidebar from "@/Components/Sidebar";
 
 export default function Home() {
   return <div></div>;
