f6ff73675f476bae5b298236dcb27217056fe823
rakadaffa <19623249@std.stei.itb.ac.id>
2024-08-25 12:54:57 +0700

    restructure: navbar

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
         <Image src="/logo-olim-km.ico" alt="logo" width={74} height={29} />
       </Link>
 
-      <ul className="xs:hidden lg:flex h-full gap-12 mx-3">
+      <ul className="hidden lg:flex h-full gap-12 mx-3">
         {NAV_LINKS.map((link) => (
           <Link
             href={link.href}
