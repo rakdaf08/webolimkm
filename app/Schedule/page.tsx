3b25f9ada59c502b4addd4c32948105e8d7ec63f
farrelathalla <farrelxag@gmail.com>
2024-09-30 22:31:55 +0700

    feat: add schedule

--- app/Schedule/page.tsx
+++ app/Schedule/page.tsx
@@ -1,48 +1,120 @@
-import React from "react";
+"use client"
+import React, { useState } from 'react';
+import { schedule } from './schedule';
+import Image from 'next/image';
+import Bg from "@/public/schedule.png";
 
-const page = () => {
-  return (
-    <div className="grid justify-center">
-      <h1 className="font-bold text-gray-800 grid text-center">SCHEDULE</h1>
-      <div
-        role="status"
-        className="max-w-sm p-4 m-2 border border-gray-200 rounded shadow animate-pulse md:p-6"
-      >
-        <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded">
-          <svg
-            className="w-10 h-10 text-gray-200 dark:text-gray-600"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 16 20"
-          >
-            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
-            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
-          </svg>
+// Define the types for the match and schedule
+interface Match {
+    day: number;
+    tanggal: string;
+    lokasi: string;
+    sportName: string;
+    waktu: string;
+    tim1: string;
+    image1: string;
+    skor1: string;
+    tim2: string;
+    image2: string;
+    skor2: string;
+}
+
+interface Schedule {
+    week: number;
+    match: Match[];
+}
+
+const SchedulePage: React.FC = () => {
+    const [selectedWeek, setSelectedWeek] = useState(1); // Default to Week 1
+
+    const handleWeekChange = (week: number) => {
+        setSelectedWeek(week);
+    };
+
+    // Extract matches for the selected week
+    const matchesByDate = schedule
+        .filter((s: Schedule) => s.week === selectedWeek)
+        .flatMap((s: Schedule) => s.match)
+        .reduce((acc: Record<string, Match[]>, match: Match) => {
+            if (!acc[match.tanggal]) {
+                acc[match.tanggal] = [];
+            }
+            acc[match.tanggal].push(match);
+            return acc;
+        }, {});
+
+    // Convert the object back to an array for rendering
+    const dates = Object.keys(matchesByDate);
+
+    return (
+      <section>
+        <div className='w-full min-h-screen h-full bg-[#F9F3BA]'>
+          <Image 
+            src={Bg}
+            alt='Bg'
+            className='w-full'
+          />
+          <div className="p-4 md:p-10">
+            <div className="flex space-x-4 mb-6">
+                {[1, 2, 3, 4].map((week) => (
+                    <button
+                        key={week}
+                        onClick={() => handleWeekChange(week)}
+                        className={`px-4 py-1 text-xs sm:text-base md:text-lg rounded-xl bonjour-font ${
+                            selectedWeek === week ? 'bg-[#1D48B9] text-white' : 'bg-white text-[#1D48B9] border-[3px] border-[#1D48B9]'
+                        }`}
+                    >
+                        Week {week}
+                    </button>
+                ))}
+            </div>
+
+            <div className="space-y-4">
+                {dates.map((date) => (
+                    <div key={date}>
+                        <h2 className="text-3xl bonjour-font mb-2">{date}</h2>
+                        <div className='grid md:grid-cols-2 gap-3'>
+                          {matchesByDate[date].map((match, index) => (
+                              <div key={index} className="p-4 px-7 rounded-xl bg-[#F6E091] bonjour-font">
+                                  <h3 className="text-2xl sm:text-3xl text-[#102F8E]">{match.sportName}</h3>
+                                  <p className="text-sm sm:text-lg text-[#102F8E]">Lokasi: {match.lokasi}</p>
+                                  <hr className='border-[#102F8E] border-[1.5px] my-1'></hr>
+                                  <div className='flex mt-4 w-full'>
+                                    <div className='w-[15%] flex flex-col items-center xs:border-r-2 xs:border-[#102F8E] px-2'>
+                                      <p className="text-[#102F8E] text-sm xs:text-xl sm:text-2xl md:text-xl lg:text-3xl">{match.waktu}</p>
+                                      <p className="text-[#102F8E] text-xs xs:text-base md:text-sm lg:text-lg">WIB</p>
+                                    </div>
+                                    <div className='w-[85%] flex flex-col space-y-2 pl-4 justify-center text-[#102F8E] text-xs xs:text-base sm:text-lg md:text-base lg:text-lg'>
+                                      <div className='flex justify-between'>
+                                        <div className='flex gap-1'>
+                                          <Image src={match.image1} width={20} height={20} alt="Logo"/>
+                                          <p> {match.tim1} </p>
+                                        </div>
+                                        <div>
+                                          <p> {match.skor1} </p>
+                                        </div>
+                                      </div>
+                                      <div className='flex justify-between'>
+                                        <div className='flex gap-1'>
+                                          <Image src={match.image2} width={20} height={20} alt="Logo"/>
+                                          <p> {match.tim2} </p>
+                                        </div>
+                                        <div>
+                                          <p> {match.skor2} </p>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                              </div>
+                          ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-        <div className="h-2.5 bg-gray-200 rounded-full"></div>
-        <div className="h-2 bg-gray-200 rounded-full"></div>
-        <div className="h-2 bg-gray-200 rounded-full"></div>
-        <div className="h-2 bg-gray-200 rounded-full"></div>
-        <div className="flex items-center mt-4">
-          <svg
-            className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-          >
-            <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-          </svg>
-          <div>
-            <div className="h-2.5 bg-gray-200 rounded-full"></div>
-            <div className="w-48 h-2 bg-gray-200 rounded-full"></div>
-          </div>
         </div>
-        <span className="sr-only">Loading...</span>
-      </div>
-    </div>
-  );
+      </section>
+    );
 };
 
-export default page;
+export default SchedulePage;
