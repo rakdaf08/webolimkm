d551fb177a400b9966b303c656ec2666a1fe0f25
maymilla <164962489+MaylaYL@users.noreply.github.com>
2024-11-03 00:03:58 +0700

    add: atletik, voli, tenis meja schedule

--- app/Schedule/Renang/schedule.ts
+++ app/Schedule/Renang/schedule.ts
@@ -2,7 +2,192 @@ export const schedule = [
     {
         round: 1,
         match : [
-            
+            [
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 1",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Tambang",
+                    image2: "./HMT.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 1",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Teknik Material",
+                    image2: "./MTM.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 1",
+                    skor1: " ",
+                    tim2: "Ikatan Mahasiswa Geodesi",
+                    image2: "./IMG.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 1",
+                    skor1: " ",
+                    tim2: "Keluarga Mahasiswa Seni Rupa",
+                    image2: "./KMSR.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 2",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Informatika",
+                    image2: "./HMIF.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 2",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Oseanografi",
+                    image2: "./HMO TRITON.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 2",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Teknik Lingkungan",
+                    image2: "./HMTL.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 2",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Teknik Material",
+                    image2: "./MTM.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 3",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Teknik Lingkungan",
+                    image2: "./HMTL.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 3",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Sipil",
+                    image2: "./HMS.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 3",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Tambang",
+                    image2: "./HMT.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 4",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Sipil",
+                    image2: "./HMS.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 4",
+                    skor1: " ",
+                    tim2: "Ikatan Mahasiswa Metalurgi",
+                    image2: "./IMMG.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 4",
+                    skor1: " ",
+                    tim2: "Keluarga Mahasiswa Teknik Penerbangan",
+                    image2: "./KMPN.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 5",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Oseanografi",
+                    image2: "./HMO TRITON.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 5",
+                    skor1: " ",
+                    tim2: "Keluarga Mahasiswa Teknik Penerbangan",
+                    image2: "./KMPN.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 5",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Fisika Teknik",
+                    image2: "./HMFT.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 6",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Farmasi 'Ars Praeparandi'",
+                    image2: "./HMF AP.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 6",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Fisika Teknik",
+                    image2: "./HMFT.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 6",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Matematika",
+                    image2: "./HIMATIKA.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 7",
+                    skor1: " ",
+                    tim2: "Ikatan Mahasiswa Metalurgi",
+                    image2: "./IMMG.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 7",
+                    skor1: " ",
+                    tim2: "Keluarga Mahasiswa Teknik Industri",
+                    image2: "./MTI.png",
+                    skor2: " "
+                },
+                {
+                    sportName: "Atletik Putri",
+                    tim1: "Grup 7",
+                    skor1: " ",
+                    tim2: "Himpunan Mahasiswa Biologi",
+                    image2: "./HIMABIO Nymphaea.png",
+                    skor2: " "
+                }
+            ]
         ],
     },
     {
@@ -11,10 +196,10 @@ export const schedule = [
             {
                 sportName: "Futsal",
                 tim1: "Himpunan Mahasiswa Informatika",
-                image1: "/HMIF.svg",
+                image1: "../HMIF.svg",
                 skor1: "4",
                 tim2: "Himpunan Mahasiswa Elektroteknik",
-                image2: "/HMIF.svg",
+                image2: "./HMIF.svg",
                 skor2: "1",
             },
         ],
--- app/Schedule/Tenis Meja/schedule.ts
+++ app/Schedule/Tenis Meja/schedule.ts
@@ -2,21 +2,113 @@ export const schedule = [
     {
         round: 1,
         match : [
-            
+            [
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Teknik Geofisika 'Terra'",
+                    image1: "./HMTG.png",
+                    skor1: "0",
+                    tim2: "Himpunan Mahasiswa Teknik Lingkungan",
+                    image2: "./HMTL.png",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Informatika",
+                    image1: "./HMIF.png",
+                    skor1: "0",
+                    tim2: "Himpunan Mahasiswa Biologi 'Nymphaea'",
+                    image2: "./HIMABIO.png",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Oseanografi 'Triton'",
+                    image1: "./HMO.png",
+                    skor1: "0",
+                    tim2: "Himpunan Mahasiswa Sipil",
+                    image2: "./HMS.png",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Teknik Perminyakan 'Patra'",
+                    image1: "./HMTM.png",
+                    skor1: "0",
+                    tim2: " ",
+                    image2: "./ ",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Tambang",
+                    image1: "./HMT.png",
+                    skor1: "0",
+                    tim2: "Himpunan Mahasiswa Astronomi",
+                    image2: "./Himastron.png",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Keluarga Mahasiswa Teknik Industri",
+                    image1: "./MTI.png",
+                    skor1: "0",
+                    tim2: "Keluarga Mahasiswa Teknik Kelautan",
+                    image2: "./KMKL.png",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Elektroteknik",
+                    image1: "./HME.png",
+                    skor1: "0",
+                    tim2: "Himpunan Mahasiswa Teknik Perencanaan Wilayah dan Kota 'Pangripta Loka'",
+                    image2: "./HMP.png",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Fisika Teknik",
+                    image1: "./HMFT.png",
+                    skor1: "0",
+                    tim2: " ",
+                    image2: "./ ",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Teknik Material",
+                    image1: "./MTM.png",
+                    skor1: "0",
+                    tim2: "Himpunan Mahasiswa Teknik Kimia",
+                    image2: "./HIMATEK.png",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Keluarga Mahasiswa Teknik Penerbangan",
+                    image1: "./KMPN.png",
+                    skor1: "0",
+                    tim2: " ",
+                    image2: "./ ",
+                    skor2: "0"
+                },
+                {
+                    sportName: "Tenis Meja",
+                    tim1: "Himpunan Mahasiswa Mesin",
+                    image1: "./HMM.png",
+                    skor1: "0",
+                    tim2: "Ikatan Mahasiswa Metalurgi",
+                    image2: "./IMMG.png",
+                    skor2: "0"
+                }
+            ]
         ],
     },
     {
         round: 2,
         match : [
-            {
-                sportName: "Futsal",
-                tim1: "Himpunan Mahasiswa Informatika",
-                image1: "/HMIF.svg",
-                skor1: "4",
-                tim2: "Himpunan Mahasiswa Elektroteknik",
-                image2: "/HMIF.svg",
-                skor2: "1",
-            },
+            
         ],
     },
   ]
