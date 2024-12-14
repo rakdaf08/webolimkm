db84bdfc4eecb9f7ea6a0465bdd0b95d4fd30b7d
rakadaffa <19623249@std.stei.itb.ac.id>
2024-12-15 02:35:24 +0700

    update: schedule

--- app/Schedule/BasketPutra/schedule.ts
+++ app/Schedule/BasketPutra/schedule.ts
@@ -6,136 +6,136 @@ export const schedule = [
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Elektroteknik (A)",
                 image1: "/LogoHMPS/HME.png",
-                skor1: "0",
+                skor1: "23",
                 tim2: 'Himpunan Mahasiswa Teknik Geologi "GEA"',
                 image2: "/LogoHMPS/HMTG.png",
-                skor2: "0"
+                skor2: "16"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Ikatan Mahasiswa Geodesi",
                 image1: "/LogoHMPS/IMG.png",
-                skor1: "0",
+                skor1: "22",
                 tim2: "Himpunan Mahasiswa Tambang",
                 image2: "/LogoHMPS/HMT.png",
-                skor2: "0"
+                skor2: "23"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Ikatan Mahasiswa Arsitektur Gunadharma",
                 image1: "/LogoHMPS/IMA_G.png",
-                skor1: "0",
+                skor1: "9",
                 tim2: 'Ikatan Mahasiswa Kewirausahaan "Artha"',
                 image2: "/LogoHMPS/IMK.png",
-                skor2: "0"
+                skor2: "23"
             },
             {
                 sportName: "Basket Putra",
                 tim1: 'Himpunan Mahasiswa Teknik Perminyakan "PATRA"',
                 image1: "/LogoHMPS/HMTM.png",
-                skor1: "0",
+                skor1: "57",
                 tim2: 'Himpunan Mahasiswa Meteorologi "Atmosphaira"',
                 image2: "/LogoHMPS/HMME.png",
-                skor2: "0"
+                skor2: "8"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Ikatan Mahasiswa Metalurgi",
                 image1: "/LogoHMPS/IMMG.png",
-                skor1: "0",
+                skor1: "30",
                 tim2: "Himpunan Mahasiswa Farmasi 'Ars Praeparandi''",
                 image2: "/LogoHMPS/HMF.png",
-                skor2: "0"
+                skor2: "11"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Teknik Lingkungan",
                 image1: "/LogoHMPS/HMTL.png",
-                skor1: "0",
+                skor1: "14",
                 tim2: 'Himpunan Mahasiswa Oseanografi "TRITON"',
                 image2: "/LogoHMPS/HMO.png",
-                skor2: "0"
+                skor2: "13"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Mesin",
                 image1: "/LogoHMPS/HMM.png",
-                skor1: "0",
+                skor1: "31",
                 tim2: "Himpunan Mahasiswa Fisika Teknik",
                 image2: "/LogoHMPS/HMFT.png",
-                skor2: "0"
+                skor2: "11"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Perencanaan Wilayah dan Kota 'Pangripta Loka'",
                 image1: "/LogoHMPS/HMP.png",
-                skor1: "0",
+                skor1: "WIN",
                 tim2: 'Himpunan Mahasiswa Biologi "Nymphaea"',
                 image2: "/LogoHMPS/HIMABIO.png",
-                skor2: "0"
+                skor2: "WO"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Matematika",
                 image1: "/LogoHMPS/HIMATIKA.png",
-                skor1: "0",
+                skor1: "10",
                 tim2: "Keluarga Mahasiswa Teknik Kelautan",
                 image2: "/LogoHMPS/KMKL.png",
-                skor2: "0"
+                skor2: "9"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Teknik Material",
                 image1: "/LogoHMPS/MTM.png",
-                skor1: "0",
+                skor1: "23",
                 tim2: "Himpunan Mahasiswa Informatika",
                 image2: "/LogoHMPS/HMIF.png",
-                skor2: "0"
+                skor2: "20"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Keluarga Mahasiswa Seni Rupa",
                 image1: "/LogoHMPS/KMSR.png",
-                skor1: "0",
+                skor1: "11",
                 tim2: "Keluarga Mahasiswa Manajemen",
                 image2: "/LogoHMPS/KMM.png",
-                skor2: "0"
+                skor2: "37"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Teknik Kimia",
                 image1: "/LogoHMPS/HIMATEK.png",
-                skor1: "0",
+                skor1: "23",
                 tim2: "Himpunan Mahasiswa Fisika",
                 image2: "/LogoHMPS/HIMAFI.png",
-                skor2: "0"
+                skor2: "15"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Sipil",
                 image1: "/LogoHMPS/HMS.png",
-                skor1: "0",
+                skor1: "25",
                 tim2: 'Himpunan Mahasiswa Teknik Geofisika "TERRA"',
                 image2: "/LogoHMPS/HIMA_TG.png",
-                skor2: "0"
+                skor2: "11"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Keluarga Mahasiswa Teknik Penerbangan",
                 image1: "/LogoHMPS/KMPN.png",
-                skor1: "0",
+                skor1: "25",
                 tim2: 'Ikatan Mahasiswa Telekomunikasi "Signum"',
                 image2: "/LogoHMPS/IMT.png",
-                skor2: "0"
+                skor2: "21"
             },
             {
                 sportName: "Basket Putra",
                 tim1: "Himpunan Mahasiswa Elektroteknik (B)",
                 image1: "/LogoHMPS/HME.png",
-                skor1: "0",
+                skor1: "2",
                 tim2: "Keluarga Mahasiswa Teknik Industri",
                 image2: "/LogoHMPS/MTI.png",
-                skor2: "0"
+                skor2: "37"
             }]
     },
     {
@@ -143,66 +143,66 @@ export const schedule = [
         match : [
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Himpunan Mahasiswa Elektroteknik (A)",
+                image1: "/LogoHMPS/HME.png",
+                skor1: "26",
+                tim2: "Himpunan Mahasiswa Tambang",
+                image2: "/LogoHMPS/HMT.png",
+                skor2: "23"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: 'Ikatan Mahasiswa Kewirausahaan "Artha"',
+                image1: "/LogoHMPS/IMK.png",
+                skor1: "17",
+                tim2: 'Himpunan Mahasiswa Teknik Perminyakan "PATRA"',
+                image2: "/LogoHMPS/HMTM.png",
+                skor2: "28"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Ikatan Mahasiswa Metalurgi",
+                image1: "/LogoHMPS/IMMG.png",
+                skor1: "30",
+                tim2: "Himpunan Mahasiswa Teknik Lingkungan",
+                image2: "/LogoHMPS/HMTL.png",
+                skor2: "22"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Himpunan Mahasiswa Mesin",
+                image1: "/LogoHMPS/HMM.png",
+                skor1: "26",
+                tim2: "Himpunan Mahasiswa Perencanaan Wilayah dan Kota 'Pangripta Loka'",
+                image2: "/LogoHMPS/HMP.png",
+                skor2: "12"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Himpunan Mahasiswa Matematika",
+                image1: "/LogoHMPS/HIMATIKA.png",
+                skor1: "13",
+                tim2: "Himpunan Mahasiswa Teknik Material",
+                image2: "/LogoHMPS/MTM.png",
+                skor2: "23"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Keluarga Mahasiswa Manajemen",
+                image1: "/LogoHMPS/KMM.png",
+                skor1: "41",
+                tim2: "Himpunan Mahasiswa Teknik Kimia",
+                image2: "/LogoHMPS/HIMATEK.png",
+                skor2: "16"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Himpunan Mahasiswa Sipil",
+                image1: "/LogoHMPS/HMS.png",
+                skor1: "28",
+                tim2: "Keluarga Mahasiswa Teknik Penerbangan",
+                image2: "/LogoHMPS/KMPN.png",
+                skor2: "23"
             }]
     },
     {
@@ -210,39 +210,39 @@ export const schedule = [
         match : [
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
+                tim1: "Himpunan Mahasiswa Elektroteknik (A)",
+                image1: "/LogoHMPS/HME.png",
                 skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
+                tim2: 'Himpunan Mahasiswa Teknik Perminyakan "PATRA"',
+                image2: "/LogoHMPS/HMTM.png",
                 skor2: "0"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Ikatan Mahasiswa Metalurgi",
+                image1: "/LogoHMPS/IMMG.png",
+                skor1: "19",
+                tim2: "Himpunan Mahasiswa Mesin",
+                image2: "/LogoHMPS/HMM.png",
+                skor2: "34"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Himpunan Mahasiswa Teknik Material",
+                image1: "/LogoHMPS/MTM.png",
+                skor1: "18",
+                tim2: "Keluarga Mahasiswa Manajemen",
+                image2: "/LogoHMPS/KMM.png",
+                skor2: "35"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
-                skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
-                skor2: "0"
+                tim1: "Himpunan Mahasiswa Sipil",
+                image1: "/LogoHMPS/HMS.png",
+                skor1: "18",
+                tim2: "Keluarga Mahasiswa Teknik Industri",
+                image2: "/LogoHMPS/MTI.png",
+                skor2: "27"
             }]
     },
     {
@@ -253,17 +253,17 @@ export const schedule = [
                 tim1: "TBD",
                 image1: "/TBD.png",
                 skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
+                tim2: "Himpunan Mahasiswa Mesin",
+                image2: "/LogoHMPS/HMM.png",
                 skor2: "0"
             },
             {
                 sportName: "Basket Putra",
-                tim1: "TBD",
-                image1: "/TBD.png",
+                tim1: "Keluarga Mahasiswa Manajemen",
+                image1: "/LogoHMPS/KMM.png",
                 skor1: "0",
-                tim2: "TBD",
-                image2: "/TBD.png",
+                tim2: "Keluarga Mahasiswa Teknik Industri",
+                image2: "/LogoHMPS/MTI.png",
                 skor2: "0"
             }]
     },
